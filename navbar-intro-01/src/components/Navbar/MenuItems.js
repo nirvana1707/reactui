b2ec60c66a9bf1b14e5ab6653bd6ef7b9843150c
Shebin <shebinscaria@yahoo.co.in>
2021-03-07 22:27:05 +0530

    Added scripts/codes

---
+++ navbar-intro-01/src/components/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+// array - all menu names here
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#', // www.google.com
+        cName: 'nav-links' // shortname - way to import classNames without import statement
+    },
+    {
+        title: 'Services',
+        url: '#', // www.google.com
+        cName: 'nav-links' // shortname - way to import classNames without import statement
+    },
+    {
+        title: 'Products',
+        url: '#', // www.google.com
+        cName: 'nav-links' // shortname - way to import classNames without import statement
+    },
+    {
+        title: 'Contact Us',
+        url: '#', // www.google.com
+        cName: 'nav-links' // shortname - way to import classNames without import statement
+    },
+    {
+        title: 'Sign up',
+        url: '#', // www.google.com
+        cName: 'nav-links-mobile' // shortname - way to import classNames without import statement
+    }
+]
+
